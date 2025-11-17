3cac0d3f54f59f57f938080ca732d503e6db2ff7
Norbert Oliszewski <norbert.oliszewski@edu.uekat.pl>
2025-11-17 13:03:19 +0100

    Delete zad_4.py

--- zad_4.py
+++
@@ -1,7 +0,0 @@
-def display_only_second_element(numbers):
-    for i in range(0, len(numbers), 2):
-        print(numbers[i])
-
-numbers = [ 61, 54, 21, 99, 88, 72, 44, 30, 40, 12]
-
-display_only_second_element(numbers)
