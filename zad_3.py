31a686a20061b47b6c9eefe32228cd007654840d
Norbert Oliszewski <norbert.oliszewski@edu.uekat.pl>
2025-11-17 13:03:07 +0100

    Delete zad_3.py

--- zad_3.py
+++
@@ -1,8 +0,0 @@
-def display_even_numbers(numbers3):
-    for number3 in numbers3:
-        if number3 % 2 == 0:
-            print(number3)
-
-numbers_list3 = list(range(10))
-print("C) Liczby parzyste w numbers_list3:")
-display_even_numbers(numbers_list3)
